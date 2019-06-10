2c82d7adcfc672f3ff8cb42618fc0969f7cde9cf
Joo-Ho Lee <jooho_lee@outlook.kr>
2019-06-10 17:00:12 +0900

    INIT: Init project

---
+++ functions/index.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Text, Card, Image, Suggestion, Payload} = require('dialogflow-fulfillment');
+
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+
+// keis link and image URLs
+const keisLogoImageUrl = 'https://www.keis.or.kr/site/main/images/common/logo.png';
+
+
